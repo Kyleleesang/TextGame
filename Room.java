a4652c16023859210e6d6ffd6aab477c88ffa081
Kyleleesang <51241586+Kyleleesang@users.noreply.github.com>
2019-09-17 10:58:34 -0400

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+import java.lang;
+import java.string;
+
+/**
+ *
+ * @author kleesans
+ */
+public class Room {
+    string name;
+    animal Animals[];
+    
+    public String toString(){
+  for (int i = 0; i < Animals.length; i++;){
+          System.out.println(Animal.toString());
+    }
+  }
+  
+  public void addAnimal(Animal a) {
+      for (int i = 0; i < Animals.length; i++;){
+      if (Animals[i] == null){
+      Animals[i] == new animal;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ animal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+
+/**
+ *
+ * @author kleesans
+ */
+public class animal {
+    string name;
+    Room room;
+    
+    public string toString(){
+        return name;
+    }
+    
+    public void newAnimal(string DesiredName){
+        new animal(DesiredName);
+    }
+    
+}
